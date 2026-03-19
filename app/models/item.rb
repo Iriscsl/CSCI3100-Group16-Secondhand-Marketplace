419a59dd6863816fe5d5695dc350c0621881bf10
Iriscsl <1155212944@link.cuhk.edu.hk>
2026-03-19 22:57:46 +0800

    Fix lint errors in item model comments

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,5 @@
 class Item < ApplicationRecord
    # belongs_to :user
    # enum status: [:available, :reserved, :sold]
-   #enum :status, [:available, :reserved, :sold]
+   # enum :status, [:available, :reserved, :sold]
 end
