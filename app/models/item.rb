8bc0f259dd14501dbc30075146c5d6effd70291a
Iriscsl <1155212944@link.cuhk.edu.hk>
2026-03-26 00:26:29 +0800

    Comment out belongs_to :user until User model exists

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,4 @@
 class Item < ApplicationRecord
   # belongs_to :user
-  # enum status: [:available, :reserved, :sold]
-  # enum :status, [:available, :reserved, :sold]
+  enum :status, [:available, :reserved, :sold]
 end
