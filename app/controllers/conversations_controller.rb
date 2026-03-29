66af62f0890dd4d6f3088e6b2e4389ceb007eb79
leungvanice <leungvanice@gmail.com>
2026-03-29 18:24:07 +0800

    add butotn link to conversation in the item show page

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,10 +1,30 @@
 class ConversationsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_conversation, only: [:show]
+
+  def index
+    @conversations = Conversation.where("buyer_id = ? OR seller_id = ?", current_user.id, current_user.id)
+                                 .order(updated_at: :desc)
+  end
+
   def show
+    @messages = @conversation.messages.order(:created_at)
+    @message = @conversation.messages.build
   end
 
   def create
+    @conversation = Conversation.find_or_create_by(
+      item_id: params[:item_id],
+      buyer_id: current_user.id,
+      seller_id: params[:seller_id]
+    )
+
+    redirect_to conversation_path(@conversation)
   end
 
-  def index
+  private
+
+  def set_conversation
+    @conversation = Conversation.find(params[:id])
   end
 end
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,27 @@
 class MessagesController < ApplicationController
+  before_action :authenticate_user! 
+  before_action :set_conversation 
+
   def create
+    @message = @convesration.messages.build(message_params)
+    @message.sender = current_user 
+
+    if @message.save 
+      redirect_to conversation_path(@convesration), notice: "Message sent." 
+    else 
+      @messages = @conversation.messages.order(:created_at) 
+      render "conversations/show", status: :unprocessable_entity 
+    end 
+  end
+
+  private
+
+  def set_conversation 
+    @convesrsation = Conversation.find(params[:conversation_id])
+  end
+
+  def message_params 
+    params.require(:message).permit(:content)
+    # make sure only accepting { message: {content: "helllo"} }
   end
 end
