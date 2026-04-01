8018184c0f6a6d07e3f681ed2a63a09459783f51
leungvanice <leungvanice@gmail.com>
2026-04-01 12:15:14 +0800

    realtime

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,17 @@
 class Message < ApplicationRecord
   belongs_to :conversation
   belongs_to :sender, class_name: "User"
+
+  after_create_commit :broadcast_message 
+
+  private 
+  
+  def broadcast_message
+    broadcast_append_to(
+      conversation,
+      target: "messages",
+      partial: "messages/message",
+      locals: { message: self }
+    )
+  end
 end
