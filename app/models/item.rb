171d40c529ced31d19225a39230ea8e5958f157a
sheena <sheenachann@gmail.com>
2026-04-03 17:51:57 +0800

    Add fuzzy search

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
   # belongs_to :user
   include PgSearch::Model
-  enum :status, [ :available, :reserved, :sold ]
+  enum :status, [:available, :reserved, :sold]
 
   enum :community, {
     chung_chi: 0,
